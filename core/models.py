48e511d1fa1c0256fd4ba111b43b7c8662c92eee
AlStien <aj601604@gmail.com>
2023-12-31 18:14:20 +0530

    feat: added models and core app for the project

---
+++ core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
+
+User = get_user_model()
+
+# Create your models here.
+class BaseModel(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta(object):
+        abstract = True  # this is an abstract model
+
+    def is_deleted(self, *args, **kwargs):
+        return self.deleted is not None
+
+class UserAlerts(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='alert')
+    price = models.PositiveIntegerField()
+    triggered = models.BooleanField(default=False)
